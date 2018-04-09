114b65a25d8698478831564ab439b02414c9ff94
cK <wim.va@mail.com>
2018-04-09 13:27:09 +0200

    random array & mild refectoring

--- RGBColorGame/colorGame.js
+++ RGBColorGame/colorGame.js
@@ -1,14 +1,18 @@
-var colors = [
+/* var colors = [
     "rgb(0, 255, 255)",
     "rgb(255, 0, 255)",
     "rgb(255, 255, 0)",
     "rgb(0, 0, 255)",
     "rgb(255, 0, 0)",
     "rgb(0, 255, 0)"
-];
+]; */
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[Math.floor(Math.random() * colors.length)];
+var difficulty = 6;
+var colors = generateRandomColorArray(difficulty);
+var pickedColor = pickAColor();
 var colorDisplay = document.querySelector("#colorDisplay");
+var playerMessage = document.querySelector("#message");
+var title = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -17,11 +21,41 @@ for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener("click", function () {
         var clickedColor = this.style.backgroundColor;
         if (clickedColor === pickedColor) {
-            for (var i = 0; i < squares.length; i++) {
-                squares[i].style.backgroundColor = pickedColor;
-            }
+            changeColors(clickedColor);
+            // for (var i = 0; i < squares.length; i++) {
+            //     squares[i].style.backgroundColor = pickedColor;
+            // }
+            playerMessage.textContent = "You win!";
+            title.style.backgroundColor = pickedColor;
         } else {
             this.style.backgroundColor = "#232323";
+            playerMessage.textContent = "Try again!";
         }
     })
+}
+
+function changeColors(color) {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = pickedColor;
+    }
+}
+
+function pickAColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function generateRandomColorArray(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push(pickRandomColor());
+    }
+    return arr;
+}
+
+function pickRandomColor() {
+    var red = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
+
 }
