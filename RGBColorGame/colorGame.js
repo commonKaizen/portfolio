0e2b1e7c6a9dbf0e059944c5f634a807afdb34ef
cK <wim.va@mail.com>
2018-04-09 12:15:19 +0200

    square coloring game play

--- RGBColorGame/colorGame.js
+++ RGBColorGame/colorGame.js
@@ -7,11 +7,21 @@ var colors = [
     "rgb(0, 255, 0)"
 ];
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[Math.floor(Math.random()*colors.length)];
+var pickedColor = colors[Math.floor(Math.random() * colors.length)];
 var colorDisplay = document.querySelector("#colorDisplay");
 
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
+    squares[i].addEventListener("click", function () {
+        var clickedColor = this.style.backgroundColor;
+        if (clickedColor === pickedColor) {
+            for (var i = 0; i < squares.length; i++) {
+                squares[i].style.backgroundColor = pickedColor;
+            }
+        } else {
+            this.style.backgroundColor = "#232323";
+        }
+    })
 }
